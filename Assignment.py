efeeca09ab46ed620354eb61118b61ab651530ee
mandark <rzmeeker@gmail.com>
2020-03-28 10:57:15 -0400

    implemented assignment cache lookup

--- Assignment.py
+++ Assignment.py
@@ -7,15 +7,14 @@ class Assignment:
     classroom_service = get_classroom_service()
 
     def __init__(self, assignmentId):
-        self.id = assignmentId
+        self.id = str(assignmentId)
+        self.service = Assignment.classroom_service
         self.json = self.get_json()
 
 
     def get_json(self):
         if self.is_cached():
-            dir = os.path.dirname(os.path.realpath(__file__))
-            filename = os.path.join(dir, 'cache', 'assignments', f'{self.id}.json')
-            with open(filename, 'r') as jsonfile:
+            with open(self.is_cached(), 'r') as jsonfile:
                 print(f'Retrieved assignment from cache: {self.id}')
                 return json.load(jsonfile)
         else:
@@ -24,3 +23,14 @@ class Assignment:
                 print(assignments_packed)
             else:
                 return None
+
+    def is_cached(self):
+        dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'cache', 'assignments')
+        for root, dirs, files in os.walk(dir):
+            for name in files:
+                print(name)
+                if self.id in name:
+                    print(f'Found cached as {name}')
+                    return os.path.join(root, name)
+        print('Not found in cache')
+        return False
