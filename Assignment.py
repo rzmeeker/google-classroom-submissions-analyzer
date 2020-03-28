b4903dbd2a1e32f3c04583e7756463649934a643
mandark <rzmeeker@gmail.com>
2020-03-27 23:17:28 -0400

    Assignment class initial commit

--- Assignment.py
+++ Assignment.py
@@ -1,3 +1,26 @@
+from service import get_classroom_service
+import os
+import json
+
 class Assignment:
-    def __int__(self):
-        pass
+
+    classroom_service = get_classroom_service()
+
+    def __init__(self, assignmentId):
+        self.id = assignmentId
+        self.json = self.get_json()
+
+
+    def get_json(self):
+        if self.is_cached():
+            dir = os.path.dirname(os.path.realpath(__file__))
+            filename = os.path.join(dir, 'cache', 'assignments', f'{self.id}.json')
+            with open(filename, 'r') as jsonfile:
+                print(f'Retrieved assignment from cache: {self.id}')
+                return json.load(jsonfile)
+        else:
+            assignments_packed = self.service.courses().courseWork().get(courseId=self.id, id=self.id).execute()
+            if assignments_packed != {}:
+                print(assignments_packed)
+            else:
+                return None
