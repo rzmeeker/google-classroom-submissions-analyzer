ba1c0ce72605f5077b1a40666213c7e03828a275
mandark <rzmeeker@gmail.com>
2020-03-28 12:25:55 -0400

    converted to class based implementation

--- app.py
+++ app.py
@@ -10,12 +10,11 @@ submission_count_dict = {}
 
 def add_count(user, submitted):
     if user not in submission_count_dict.keys():
-        submission_count_dict[user] = {'name': get_user_email_from_id(submission['userId']),
+        submission_count_dict[user] = {'name': get_user_email_from_id(user),
                                        'complete': 0,
                                        'max': 0}
     submission_count_dict[user]['complete'] += submitted
     submission_count_dict[user]['max'] += 1
-
 
 
 for course in abelCourses:
@@ -23,29 +22,21 @@ for course in abelCourses:
         print(course.name, 'retrieved from cache')
     else:
         course.save_to_cache()
-        print(course.name, 'saved to cache for next time!')
+        print(course.name, 'cached for next time!')
     if course.assignments is not None:
         if course.assignments_cached() is False:
-            print('Assignments being cached now.')
+            print('Caching Assignments')
             course.cache_all_assignments()
         else:
-            print('Assignments already cached.')
-        for assignment in course.assignments:
-            #print(assignment)
-            print(assignment['id'])
-            submissions = course.get_submissions(assignment['id'])
-            for submission in submissions:
-                #print(f"For assignment:{assignment['title']} in course: {course.name}")
-                #print(f"{get_user_email_from_id(submission['userId'])} has state {submission['state']}")
-                if submission['state'] == 'TURNED_IN' or submission['state'] == 'RETURNED':
-                    add_count(user=submission['userId'], submitted=1)
+            print(f'Assignments already cached for {course.name}')
+        for a in course.assignments:
+            for s in a.submissions:
+                if s.state == 'TURNED_IN' or s.state == 'RETURNED':
+                    add_count(user=s.studentId, submitted=1)
                 else:
-                    add_count(user=submission['userId'], submitted=0)
-            #input('Press Enter for next assignment')
+                    add_count(user=s.studentId, submitted=0)
 
 
-
-    #input('Press Enter for next course')
 
 dir = os.path.dirname(os.path.realpath(__file__))
 filename = os.path.join(dir, 'output', f'{teacherEmail}.csv')
