50cfe0852197fcbb6a5aa430fe7e811f0692b148
mandark <rzmeeker@gmail.com>
2020-03-31 18:01:10 -0400

    implemented get_drive_service

--- service.py
+++ service.py
@@ -66,3 +66,30 @@ def get_directory_service():
     service = build('admin', 'directory_v1', credentials=creds)
     return service
 
+def get_drive_service():
+    """Shows basic usage of the Classroom API.
+    Prints the names of the first 10 courses the user has access to.
+    """
+    SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly',
+              'https://www.googleapis.com/auth/drive.file']
+    creds = None
+    # The file classroom_token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('drive_token.pickle'):
+        with open('drive_token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('drive_token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('drive', 'v3', credentials=creds)
+    return service
