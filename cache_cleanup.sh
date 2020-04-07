b7fc8e7b75289b303130f52aba4f51f2239f1142
mandark <rzmeeker@gmail.com>
2020-04-07 09:49:42 -0400

    Forgot to make the cache folder before subdirs

--- cache_cleanup.sh
+++ cache_cleanup.sh
@@ -2,6 +2,7 @@
 
 systemctl stop app
 rm -rf /var/www/gcs/app/cache
+mkdir /var/www/gcs/app/cache
 mkdir /var/www/gcs/app/cache/assignments
 mkdir /var/www/gcs/app/cache/courses
 chown -R nginx:nginx /var/www/gcs/app/cache
